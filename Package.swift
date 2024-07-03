338c31a5614ab9263206862f59d72e995cc81ddc
wangzhizhou <wangzhizhou@bytedance.com>
2024-07-03 13:17:03 +0800

    support swift >=5.8 toolchain

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -47,6 +47,5 @@ let package = Package(
         .target(name: "Utils", plugins: [
             // .plugin(name: "OrzSwiftLintBuildToolPlugin", package: "OrzSwiftLint")
         ])
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
